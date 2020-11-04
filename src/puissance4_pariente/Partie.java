7a14f422dcfc16c5d9ffd94e505181cab1c45c8f
Pariente Galit <Pariente Galit@DESKTOP-9B49TK7>
2020-11-04 14:07:47 +0100

    Essai 2

--- src/puissance4_pariente/Partie.java
+++ src/puissance4_pariente/Partie.java
@@ -12,147 +12,71 @@ TP3
  * @author Pariente Galit
  */
 public class Partie {
-
-    Joueur Listejoueurs[] = new Joueur[2];
-    Joueur JoueurCourant;
-    Grille grilleDeJeu = new Grille();
-
-    void attribuerCouleursAuxJoueurs() {
-        Random x = new Random();
-        boolean couleurDujoueur;
-        couleurDujoueur = x.nextBoolean();
-        if (couleurDujoueur) {
-            Listejoueurs[0].Couleur = "Rouge";
-            Listejoueurs[1].Couleur = "Jaune";
-        } else {
-            Listejoueurs[0].Couleur = "Jaune";
-            Listejoueurs[1].Couleur = "Rouge";
-        }
+        Joueur ListeJoueurs[] = new Joueur[2]; 
+        Grille GrilleJeu = new Grille();
+        Joueur joueurCourant;
+        
+public void initialiserPartie(){
+   
+    GrilleJeu.viderGrille();
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Entrez le nom du Joueur 1 : ");
+    Joueur Joueur1 = new Joueur(sc.nextLine());
+    System.out.println("Entrez le nom du Joueur 2 : "); 
+    Joueur Joueur2 = new Joueur(sc.nextLine());
+    
+    ListeJoueurs[0] = Joueur1;
+    ListeJoueurs[1] = Joueur2;
+    
+    attribuerCouleursAuxJoueurs();
+    System.out.println(Joueur1.Nom + "possède les jetons de couleur " + Joueur1.Couleur);
+    System.out.println(Joueur2.Nom + "possède les jetons de couleur " + Joueur2.Couleur);
+    
+    for (int i=0; i<21; i++){
+        Jeton unJeton = new Jeton(ListeJoueurs[0].Couleur);
+        Joueur1.ajouter_Jeton(unJeton);
+        Jeton AutreJeton = new Jeton(ListeJoueurs[1].Couleur);
+        Joueur2.ajouter_Jeton(AutreJeton);
     }
-
-    Joueur ProchainJoueur(Joueur un_joueur) {
-        if (Listejoueurs[0] == JoueurCourant) {
-            return Listejoueurs[1];
-        }
-        return Listejoueurs[0];
+    
+    Random rand = new Random();
+    int prems;
+    prems = rand.nextInt(1);
+    if (prems == 1){
+        System.out.println(Joueur1.Nom + "commence");
+        joueurCourant = Joueur1;
     }
-
-    void initialiserPartie() {
-        grilleDeJeu.viderGrille();
-
-        // il faut créer nos deux joueurs
-        Scanner sc = new Scanner(System.in); //Pour choisir les noms des joueurs
-        System.out.println("choisir le nom du premier Joueur : ");
-        Joueur UNJoueur = new Joueur(sc.nextLine());
-
-        System.out.println("Choisir le nom du deuxième Joueur : ");
-        Joueur AutreJoueur = new Joueur(sc.nextLine());
-
-        Listejoueurs[0] = UNJoueur; //affecter les joueurs
-        Listejoueurs[1] = AutreJoueur;
-        attribuerCouleursAuxJoueurs();
-
-        System.out.println(UNJoueur.Nom + "a la couleur " + UNJoueur.Couleur);
-        System.out.println(AutreJoueur.Nom + "a la couleur " + AutreJoueur.Couleur);
-
-        // ditribuer les jetons 
-        for (int i = 0; i < 21; i++) {
-            UNJoueur.ajouter_Jeton(new Jeton(UNJoueur.Couleur));
-        }
-        AutreJoueur.ajouter_Jeton(new Jeton(AutreJoueur.Couleur));
-
-        Random r = new Random(); // fonction aléatoire pour savoir quel joueur commence 
-        boolean premierJoueur;
-        premierJoueur = r.nextBoolean();
-        if (premierJoueur) {
-            JoueurCourant = Listejoueurs[0];
-        } else { // sinon
-            JoueurCourant = Listejoueurs[1];
-        }
-
-        // initialisation des trous noirs
-        int compteur = 0;
-        for (int i = 0; i < 5; i++) {
-            int lignedutrounoir;
-            int colonnedutrounoir;
-            lignedutrounoir = r.nextInt(6);
-            colonnedutrounoir = r.nextInt(7);
-            if (compteur < 2) {
-                if (!grilleDeJeu.placerDesintegrateur(lignedutrounoir, colonnedutrounoir)) {
-                    compteur--;
-                }
-                compteur++;
-            }
-            if (!grilleDeJeu.placerTrouNoir(lignedutrounoir, colonnedutrounoir)) {
-                i--;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            int lignedudesintegrateur;
-            int colonnedudesintegrateur;
-            lignedudesintegrateur = r.nextInt(6);
-            colonnedudesintegrateur = r.nextInt(7);
-            if (!grilleDeJeu.placerDesintegrateur(lignedudesintegrateur, colonnedudesintegrateur)) {
-                i--;
-            }
-        }
-        grilleDeJeu.afficherGrilleSurConsole();
+    else {
+        System.out.println(Joueur2.Nom + "commence");
     }
+}        
+        
+public void debuterPartie(){
+    // ajouter condition qui gagne qui perd dans autre méthode continuerpartie
+}        
 
-    int menu_joueur() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(" Plusieurs choix possibles,que voulez-vous faire ?");
-        System.out.println(" Premierement: Vous voulez jouer un jeton");
-        System.out.println(" Deuxiement: Vous voulez récupérer un jeton");
-        System.out.println(" Troisiement: Vous voulez utiliser un désintégrateur et désintegrer un jeton");
-        int optionchoisi = sc.nextInt();
-        while (optionchoisi < 1 || optionchoisi > 3) {
-            System.out.println("Erreur vous avez fait un choix qui n'existe pas ");
-            System.out.println(" Veuillez entrer un des choix possible:Premierement,Deuxiement,Troisiement");
-            optionchoisi = sc.nextInt();
-        }
-        return optionchoisi;
+public void attribuerCouleursAuxJoueurs(){
+    Random alea = new Random();
+    int ChoixJoueur;
+    ChoixJoueur = alea.nextInt(1);
+    if (ChoixJoueur == 1){
+        ListeJoueurs[0].Couleur = "Jaune";
+        ListeJoueurs[1].Couleur = "Rouge";
     }
-
-    void jouerJeton() {
-        Scanner sc = new Scanner(System.in);
-        boolean jetonjouer; //intialisation boolean
-        System.out.println(" Choisissez la colonne dans laquelle vous voulez jouer le jeton");
-        int colonnejeton = sc.nextInt();
-        colonnejeton = colonnejeton - 1; //les colonnes du tableau commencent a 0
-        while (colonnejeton < 0 || colonnejeton > 6) {
-            System.out.println(" erreur vous avez choisi un numero de colonne inexistant");
-            System.out.println(" Veuillez entrer un numéro de colonne qui existe : ");
-            colonnejeton = sc.nextInt() - 1;
-        }
-
-        jetonjouer = grilleDeJeu.ajouterJetonDansColonne(JoueurCourant, colonnejeton);
-        while (!jetonjouer) {
-            System.out.println("Impossible!La colonne est deja pleine");
-            System.out.println(" Veuillez choisir une autre colonne qui n'est pas encore remplie : ");
-            colonnejeton = sc.nextInt() - 1;
-            jetonjouer = grilleDeJeu.ajouterJetonDansColonne(JoueurCourant, colonnejeton); // on ajoute le jeton jouée dans la grille de jeu
-        }
+    else {
+        ListeJoueurs[0].Couleur = "Rouge";
+        ListeJoueurs[1].Couleur = "Jaune";
     }
+}
 
-    boolean recup_jeton() {
-        int colonnejeton;
-        int lignejeton;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Choisissez le numero de la colonne ou vous voulez recuperer le jeton : ");
-        colonnejeton = sc.nextInt() - 1;
-        while (colonnejeton < 0 || colonnejeton > 6) {
-            System.out.println(" Erreur numero de colonne inexistant ");
-            System.out.println(" Veuillez entrer un numéro de colonne qui existe : ");
-            colonnejeton = sc.nextInt() - 1;
-        }
-        System.out.println("Maintenant que nous avons le numéro de la colonne,choisissez le numéro de la ligne du jeton que vous voulez récupérer : ");
-        lignejeton = sc.nextInt() - 1;
-        while (colonnejeton < 0 || colonnejeton > 5) {
-            System.out.println("erreur numero de ligne inexistant");
-            System.out.println(" Veuillez entrer un numéro de ligne qui existe : ");
-            colonnejeton = sc.nextInt() - 1;
-        }
+public Joueur CouleurSuivante (Joueur Un_Nom){
+    if (ListeJoueurs[0] == joueurCourant){
+        return ListeJoueurs[1];
+    }
+    else {
+        return ListeJoueurs[0];
+    }
+}
 
- }
+}
